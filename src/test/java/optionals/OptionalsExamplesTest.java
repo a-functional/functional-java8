b56ddbd2eb5d1e48e01d65b0d9c723b035ac7d85
Ross Westerbur <ross@autonomic.ai>
2019-08-14 10:30:37 -0400

    Add OptionalsExamplesTest

---
+++ src/test/java/optionals/OptionalsExamplesTest.java
@@ -0,0 +1,69 @@
+package optionals;
+
+import java.time.Instant;
+import java.util.Optional;
+import org.junit.Test;
+
+public class OptionalsExamplesTest {
+
+    @Test
+    public void test1() {
+        Optional<Long> maybe;
+        String result;
+
+        Long now = Instant.now().getEpochSecond();
+        if (Instant.now().getEpochSecond() % 2 == 0) {
+            maybe = Optional.of(now);
+        } else {
+            maybe = Optional.empty();
+        }
+
+        // Bad idea! Don't call .get() on an optional if you are not sure that it exists.
+        result = "i exist: " + maybe.get();
+
+        System.out.println(result);
+    }
+
+    @Test
+    public void test2() {
+        Optional<Long> maybe;
+        String result;
+
+        Long now = Instant.now().getEpochSecond();
+        if (Instant.now().getEpochSecond() % 2 == 0) {
+            maybe = Optional.of(now);
+        } else {
+            maybe = Optional.empty();
+        }
+
+        // This .get() is safe since it is wrapped in an .isPresent() check...
+        // ...but this isn't any better than an old-fashioned null-check.
+        if (maybe.isPresent()) {
+            result = "i exist: " + maybe.get();
+        } else {
+            result = "nope";
+        }
+
+        System.out.println(result);
+    }
+
+    @Test
+    public void test3() {
+        Optional<Long> maybe;
+        String result;
+
+        Long now = Instant.now().getEpochSecond();
+        if (Instant.now().getEpochSecond() % 2 == 0) {
+            maybe = Optional.of(now);
+        } else {
+            maybe = Optional.empty();
+        }
+
+        // Use a streams-style chain instead (usually using .map() and/or .filter()):
+        result = maybe
+            .map(seconds -> "i exist: " + seconds)
+            .orElse("nope");
+
+        System.out.println(result);
+    }
+}
